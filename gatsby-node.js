43d81d390f813a2d2b3fb1e1259a37ff0554d67e
Bailey Marlow <Baileymarlow2@gmail.com>
2021-10-25 16:17:16 -0400

    wordpress state 4:16

---
+++ gatsby-node.js
@@ -0,0 +1,48 @@
+const path = require(`path`)
+const { slash } = require(`gatsby-core-utils`)
+exports.createPages = async ({ graphql, actions }) => {
+  const { createPage } = actions
+  const result = await graphql(`
+  query {
+    allWpPost {
+      edges {
+        node {
+          id
+          slug
+          title
+          content
+        }
+      }
+    }
+  }  
+  `)
+
+  const postTemplate = path.resolve(`./src/templates/post.js`)
+  result.data.allWpPost.edges.forEach(edge => {
+    createPage({
+      path: edge.node.slug,
+      component: slash(postTemplate),
+      context: {
+        id: edge.node.id,
+      },
+    })
+  })
+}
+const axios = require("axios")
+const get = endpoint => axios.get(`https://pokeapi.co/api/v2${endpoint}`)
+const getPokemonData = names =>
+  Promise.all(
+    names.map(async name => {
+      const { data: pokemon } = await get(`/pokemon/${name}`)
+      return { ...pokemon }
+    })
+  )
+
+exports.createPages = async ({ actions: { createPage } }) => {
+  const allPokemon = await getPokemonData(["mew", "ditto", "squirtle"])
+  createPage({
+    path: `/pokemon`,
+    component: require.resolve("./src/templates/all-pokemon.js"),
+    context: { allPokemon },
+  })
+}
---
+++ src/templates/post.js
@@ -0,0 +1,33 @@
+import React, { Component } from "react"
+import { graphql } from "gatsby"
+import PropTypes from "prop-types"
+class Post extends Component {
+  render() {
+    const post = this.props.data.wpPost
+    return (
+      <>
+        <h1>{post.title}</h1>
+        <div dangerouslySetInnerHTML={{ __html: post.content }} />
+      </>
+    )
+  }
+}
+Post.propTypes = {
+  data: PropTypes.object.isRequired,
+  edges: PropTypes.array,
+}
+export default Post
+export const pageQuery = graphql`
+query($slug: String) {
+  allWpPost(filter: { slug: { eq: $slug } }) {
+    edges {
+      node {
+        title
+        content
+      }
+    }
+  }
+}
+`
+
+cG9zdDox
